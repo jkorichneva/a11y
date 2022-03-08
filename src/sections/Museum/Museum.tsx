61bcd34f988d4df179ac51519fb69ceffc785105
Yana Korichneva <jkorichneva750@gmail.com>
2022-03-08 20:14:51 +0300

    Deploy gh-pages

---
+++ src/sections/Museum/Museum.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styles from './Museum.module.css';
+import t from '../../utils/translate';
+
+type MuseumProps = {
+  lang: 'RU' | 'EN';
+};
+
+function Museum(props: MuseumProps): JSX.Element {
+  const { lang } = props;
+  const [tab, setTab] = React.useState<'history' | 'buildings'>('buildings');
+  const buildingsRef = React.createRef<HTMLButtonElement>();
+  const historyRef = React.createRef<HTMLButtonElement>();
+
+  const handleKeydown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    switch (event.key) {
+      case 'Enter':
+      case 'Space':
+        setTab(tab === 'history' ? 'buildings' : 'history');
+        break;
+      case 'Up':
+      case 'ArrowUp':
+      case 'Left':
+      case 'ArrowLeft':
+      case 'Down':
+      case 'ArrowDown':
+      case 'Right':
+      case 'ArrowRight':
+      case 'Home':
+      case 'End':
+        if (tab === 'history') {
+          buildingsRef?.current?.focus();
+        } else {
+          historyRef?.current?.focus();
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <section aria-labelledby="museum-sec-h" id="museum">
+      <h2 className={styles.Header}>{t('museum', lang)}</h2>
+      <div role="tablist" aria-label="О музее">
+        <button
+          role="tab"
+          type="button"
+          className={tab === 'buildings' ? styles.TabActive : styles.Tab}
+          aria-selected={tab === 'buildings'}
+          tabIndex={tab === 'buildings' ? 0 : -1}
+          id="buildings-tab"
+          ref={buildingsRef}
+          aria-controls="buildings-tabpanel"
+          onClick={() => setTab('buildings')}
+          onKeyDown={(event) => handleKeydown(event)}
+        >
+          {t('buildings', lang)}
+        </button>
+        <button
+          role="tab"
+          type="button"
+          className={tab === 'history' ? styles.TabActive : styles.Tab}
+          aria-selected={tab === 'history'}
+          tabIndex={tab === 'history' ? 0 : -1}
+          id="history-tab"
+          ref={historyRef}
+          aria-controls="history-tabpanel"
+          onClick={() => setTab('history')}
+          onKeyDown={(event) => handleKeydown(event)}
+        >
+          {t('history', lang)}
+        </button>
+      </div>
+      <div className={styles.Panel}>
+        {tab === 'buildings' && <div aria-labelledby="buildings-tab" id="buildings-tabpanel">Тут здания</div>}
+        {tab === 'history' && <div aria-labelledby="history-tab" id="history-tabpanel">Тут история</div>}
+      </div>
+    </section>
+  );
+}
+
+export default Museum;
